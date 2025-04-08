ccc91e9f28777d2c6333b0cd74f3a7b4856cf84c
rexim <reximkut@gmail.com>
2025-04-08 15:17:42 +0700

    Add "Load Logs for Preview" button

--- gui_main.cpp
+++ gui_main.cpp
@@ -240,7 +240,27 @@ int main(int, char **) {
                     printf("Failed to load image: %s\n", outPath);
                 NFD_FreePathU8(outPath);
             } else if (result == NFD_CANCEL) {
-
+                // Do nothing
+            } else {
+                printf("Error: %s\n", NFD_GetError());
+            }
+        }
+        if (ImGui::Button("Load Logs for Preview")) {
+            nfdu8char_t *outPath = nullptr;
+            nfdu8filteritem_t filters[1] = {{"CSV files", "csv"}};
+            nfdopendialogu8args_t args = {0};
+            args.filterList = filters;
+            args.filterCount = 1;
+            // Set the parent window handle using the GLFW binding
+            //NFD_GetNativeWindowFromGLFWWindow(window, &args.parentWindow);
+            nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &args);
+            if (result == NFD_OKAY) {
+                int multiplier = 1; // TODO: some way to customize time units
+                text_overlay.messages = parseCSV(outPath, multiplier);
+                text_overlay.revalidatePreview = true;
+                NFD_FreePathU8(outPath);
+            } else if (result == NFD_CANCEL) {
+                // Do nothing
             } else {
                 printf("Error: %s\n", NFD_GetError());
             }
